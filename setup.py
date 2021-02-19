f08fc2b85db08fbf373140d152f9078eba4acd11
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2021-02-19 16:38:41 -0500

    upgrade to 3.7; remove reading in readme
    forgetting to add the README.md to the zip file is a common error source

--- setup.py
+++ setup.py
@@ -3,9 +3,6 @@
 
 from setuptools import setup, find_packages
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
-
 requirements = [
     "model-tools @ git+https://github.com/brain-score/model-tools"
 ]
@@ -14,7 +11,6 @@ setup(
     name='model-template',
     version='0.1.0',
     description="An example project for adding brain or base model implementation",
-    long_description=readme,
     author="Franziska Geiger",
     author_email='fgeiger@mit.edu',
     url='https://github.com/brain-score/brainscore_model_template',
@@ -29,7 +25,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     test_suite='tests',
 )
